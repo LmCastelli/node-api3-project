eb454714393a7b2b3a6afc8a733d92fbf47430fb
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-01 15:26:36 -0600

    Finished index.js and server.js, establishing codegrade connection

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 9000;
+
+server.listen(port, () => {
+    console.log("Api3-Project listening on 9000")
+})
